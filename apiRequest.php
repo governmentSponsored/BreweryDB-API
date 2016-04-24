aef4d72ba49c9b8a0cf5afe44e668c9482c52787
Stan Smith <stancsmith@gmail.com>
2016-04-24 11:01:23 -0700

    add google api stuff

--- apiRequest.php
+++ apiRequest.php
@@ -1,8 +1,18 @@
 <?php
-	include 'keys.php';
+	include("keys.php");
 	header('Content-Type: application/json');
-	$fullUrl = 'http://api.brewerydb.com/v2/locations?locality=Alexandria&region=Virginia&countryIsoCode=us&key='
-				. $apiKey;
+	$service = $_GET['service'];
+	if($service == 'gmap') {
+		$latlong = $_GET['latlong'];
+		$fullUrl = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' . $latlong . '&key=' . $mapsKey;
+	} else {
+		$locality = $_GET['locality'];
+		$region = $_GET['region'];
+		$country = $_GET['country'];
+		$fullUrl = 'http://api.brewerydb.com/v2/locations?locality=' . $locality . '&region=' . $region . '&countryIsoCode=' . $country . '&key=' . $breweryKey;
+		#http://api.brewerydb.com/v2/locations?locality=Alexandria&region=Virginia&countryIsoCode=us&key=YOUR_KEY
+	}
+	
 	
 	$JSON = file_get_contents($fullUrl);
 	echo $JSON;
