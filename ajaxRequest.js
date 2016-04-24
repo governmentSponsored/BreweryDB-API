6a88e3d906cb8d096ff482e6137eedd9caac9866
Stan Smith <stancsmith@gmail.com>
2016-04-24 11:02:14 -0700

    geolocation, promises, and gmap api

--- ajaxRequest.js
+++ ajaxRequest.js
@@ -1,19 +1,56 @@
 $(document).ready( function() {
-    //php file that does api requests to avoid XSS
-    var url = 'apiRequest.php',
-	dataString = '';
-
-	$.ajax({ 
-        'url': url,
-        'dataType': "json"
-    }).done(function(data) {
-    	var records = data.data,
-        current;
-        console.log(data);
-    	for(var record in records) {
-            current = records[record];
-    		dataString += '<div>' + current.brewery.name + '</div>'
-    	}
-    	$('#localBreweries').html(dataString);
-    })
+    getGeoLocation();
+    
+    //default ajax setup
+    $.ajaxSetup({
+      dataType: "json"
+    });
 });
+
+//use browser built in geolocation
+function getGeoLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        return'no geo';
+    }
+}
+
+//get the lat/long from geolocation
+function showPosition(position) {
+    let lat = position.coords.latitude,
+        long = position.coords.longitude;
+    getAddressFromLatLong(lat,long).then(getBreweryFromAddress);
+}
+
+//get all the info brewerydb needs from gmaps
+function getAddressFromLatLong(lat,long) {
+    //php file that does api requests to avoid XSS
+    let url = `apiRequest.php?latlong=${lat},${long}&service=gmap`;
+    return $.ajax({ 
+        'url': url
+    });
+}
+
+function getBreweryFromAddress(addressInfo) {
+    if(addressInfo.status === "OK") {
+            var address = addressInfo.results[0].address_components,
+                locality = address[3].short_name,
+                region = address[4].long_name,
+                country = address[5].short_name;
+                console.log(locality,region,country);
+        }
+    let url = `apiRequest.php?locality=${locality}&region=${region}&country=${country}`,
+        dataString = '';
+    $.ajax({ 
+        'url': url
+    }).done(function(data) {
+        let records = data.data,
+            current;
+        for(let record in records) {
+            current = records[record];
+            dataString += '<div>' + current.brewery.name + '</div>'
+        }
+        $('#localBreweries').html(dataString);
+    });
+}
